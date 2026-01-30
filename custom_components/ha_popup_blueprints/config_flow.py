d991d907afd49453963322851678fe58519fe3d2
Salvatore Di savio <salvatore.disavio99@gmail.com>
2026-01-30 16:04:03 +0100

    Update config_flow.py

--- custom_components/ha_popup_blueprints/config_flow.py
+++ custom_components/ha_popup_blueprints/config_flow.py
@@ -9,8 +9,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        # Single instance (evita doppioni)
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
-
         return self.async_create_entry(title="HA Popup Blueprints", data={})
